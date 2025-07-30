c31c0c49f0b8dc2d86580618161fcd25137b5e77
Andrew Alderson <andrew.alderson@outlook.com>
2025-07-30 20:07:50 +0000

    style: configure style library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import {
   ApplicationConfig,
+  inject,
   provideBrowserGlobalErrorListeners,
+  provideEnvironmentInitializer,
   provideZonelessChangeDetection,
 } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
@@ -11,5 +14,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(appRoutes),
+    provideEnvironmentInitializer(() =>
+      inject(MatIconRegistry).setDefaultFontSetClass('material-symbols-rounded')
+    ),
   ],
 };
