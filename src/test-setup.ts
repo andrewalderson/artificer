99f49e2a3450a7e9566ec01b4b7247be2ac49ed1
Andrew Alderson <andrew.alderson@outlook.com>
2025-08-25 15:50:53 -0600

    fix: use zoneless for tests

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,14 +1,20 @@
-import '@analogjs/vitest-angular/setup-zone';
+import '@analogjs/vitest-angular/setup-snapshots';
 import '@angular/compiler';
 import '@testing-library/jest-dom/vitest';
 
+import { NgModule, provideZonelessChangeDetection } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserTestingModule,
   platformBrowserTesting,
 } from '@angular/platform-browser/testing';
 
+@NgModule({
+  providers: [provideZonelessChangeDetection()],
+})
+export class ZonelessTestModule {}
+
 getTestBed().initTestEnvironment(
-  BrowserTestingModule,
+  [BrowserTestingModule, ZonelessTestModule],
   platformBrowserTesting()
 );
